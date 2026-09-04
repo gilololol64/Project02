5bd0b7fb9d3eaf260651f159e90dce6d9294f2ee
final submit <gradeprison@gmail.com>
2026-09-04 12:00:54 -0400

    BanckCLI merge to master

--- src/main/java/com/bankofcli/Main.java
+++ src/main/java/com/bankofcli/Main.java
@@ -1,7 +1,11 @@
 package com.bankofcli;
 
+import com.bankofcli.api.BankCLI;
+
 public class Main {
 
-    public static void main() {
+    public static void main(String[] args) {
+        BankCLI bankCLI = new BankCLI();
+        bankCLI.run();
     }
 }
