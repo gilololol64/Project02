6de7c369c6d29c6a6d5e90c91af3f055d489e7c9
LargeUmbrella <amenakploh@gmail.com>
2026-09-05 08:55:33 -0500

    Database manager+Account repository
    Unclear of typing and waiting for Logback to do transaction repository class

--- src/main/java/com/bankofcli/database/DatabaseManager.java
+++ src/main/java/com/bankofcli/database/DatabaseManager.java
@@ -1,4 +1,57 @@
 package com.bankofcli.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManager {
+	
+	private static final String url="jdbc:sqlite:BigBankersBank.db";
+	
+	public Connection open() {
+		//returns a open connection to the SQLite Database
+		try {
+		return DriverManager.getConnection(url);
+		}catch(SQLException e) {
+			System.out.print("failure: " + e.getMessage());
+	        throw new RuntimeException("Could not connect to database", e);
+		}
+	}
+	public void close(Connection current) {
+		//closes the connection to the database
+		try {
+			current.close();
+		} catch (SQLException e) {
+			 throw new RuntimeException("Could not close connection", e);
+		}
+		
+	}
+	
+	public void init() {
+		var sqlCreateAccount ="CREATE TABLE IF NOT EXISTS accounts ("
+						+ "    account_id VARCHAR PRIMARY KEY,"
+						+ "    pin INT NOT NULL,"
+						+ "    balance INT NOT NULL"
+						+ ");";
+		var sqlCreateTransactions ="CREATE TABLE IF NOT EXISTS transactions ("
+							+ "	transaction_id VARCHAR PRIMARY KEY,"
+							+ " trans_type TEXT NOT NULL CHECK (trans_type IN ('DEPOSIT', 'WITHDRAW', 'TRANSFER')),"
+							+ "	time_complete TEXT NOT NULL DEFAULT (datetime('now')),"
+							+ " amount INT NOT NULL,"
+							+ "	account_src VARCHAR NOT NULL,"
+							+ "	account_dst VARCHAR,"
+							+ "	FOREIGN KEY (account_src) REFERENCES account(account_id),"
+							+ "	FOREIGN KEY (account_dst) REFERENCES account(account_id)"
+							+ ");";
+		
+		 try (var conn = DriverManager.getConnection(url)){
+			 var stmtA = conn.createStatement();
+			 //create two tables
+			 stmtA.execute(sqlCreateAccount);
+			 stmtA.execute(sqlCreateTransactions);
+		 }catch(SQLException e){
+			 e.printStackTrace();
+		 }
+	}
+	
 }
--- src/main/java/com/bankofcli/repository/AccountRepository.java
+++ src/main/java/com/bankofcli/repository/AccountRepository.java
@@ -1,4 +1,70 @@
 package com.bankofcli.repository;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import com.bankofcli.database.DatabaseManager;
+import com.bankofcli.model.Account;
+
 public class AccountRepository {
+	
+	DatabaseManager db;
+	//creates a log
+	public AccountRepository() {
+		db = new DatabaseManager();
+	
+	}	
+	
+	public Account findById(UUID accountID) {
+		
+		var sql ="SELECT account_id,pin,balance FROM accounts WHERE account_id = ?";
+		try(var conn = db.open()){
+			//Executes select statement and returns Account variable
+			var stmt= conn.prepareStatement(sql);
+			stmt.setString(1, accountID.toString());
+			ResultSet rs =stmt.executeQuery();
+			Account found = new Account(rs.getString("account_id"), rs.getInt("pin"), rs.getDouble("balance"));
+			return found;
+		}catch(SQLException e) {
+			System.out.print("failure: " + e.getMessage());
+	        throw new RuntimeException("Could not connect to database", e);
+		}
+		
+	}
+	public void save(Account toBeSaved) {
+		var sql ="UPDATE account SET pin = ?, balance = ? WHERE = ?";
+		try(var conn =db.open()) {
+			var stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, toBeSaved.getPin());
+			stmt.setDouble(2, toBeSaved.getBalance());
+			stmt.setString(3, toBeSaved.getAccountID());
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.print("failure: " + e.getMessage());
+	        throw new RuntimeException("Could not connect to database", e);
+		}
+		
+		
+		
+	}
+	
+	public void saveNew(Account toBeSaved) {
+		var sql ="INSERT INTO accounts(account_id,pin,balance) VALUES(?,?,?)";
+		
+		try(var conn =db.open()) {
+			var stmt = conn.prepareStatement(sql);
+			stmt.setString(1, toBeSaved.getAccountID());
+			stmt.setInt(2, toBeSaved.getPin());
+			stmt.setDouble(3, toBeSaved.getBalance());
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.print("failure: " + e.getMessage());
+	        throw new RuntimeException("Could not connect to database", e);
+		}
+	}
+	
+	
+	
 }
